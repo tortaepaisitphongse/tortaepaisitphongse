e16de6d56926b59c936ed46fb4ef8aee65b0f46d
tortaepaisitphongse <tortaepaisitphongse@gmail.com>
2025-12-29 03:06:35 +0700

    Updated experiences content

---
+++ src/content.config.ts
@@ -0,0 +1,4 @@
+// We're using JSON files instead of markdown content collections
+// Defining an empty collections object prevents Astro from auto-detecting markdown files
+export const collections = {};
+
